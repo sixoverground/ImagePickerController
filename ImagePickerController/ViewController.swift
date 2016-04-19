4ce2c305e3d1668a1e1570221ddc5c5061606472
Craig Phares <craigphares@sixoverground.com>
2016-04-19 09:10:08 -0400

    Initial Commit

---
+++ ImagePickerController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePickerController
+//
+//  Created by Craig Phares on 4/19/16.
+//  Copyright © 2016 Six Overground. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
